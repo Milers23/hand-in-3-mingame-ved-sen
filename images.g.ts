8bffacbe76db1910494c4bee99e8988978a20a34
Milers23 <milers23@gmail.com>
2024-11-24 22:55:13 +0100

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "sofa":return img`
+...cccccccccccccccccc...
+..cbddddddddddddddddbc..
+.cddddddddddddddddddddc.
+.cddbbbbbbbbbbbbbbbbddc.
+.cdbbbbbbbbbbbbbbbbbbdc.
+.cdbbbbbbbbbbbbbbbbbbdc.
+cbbbccccccccccccccccbbbc
+cddcbddddddddddddddbcddc
+cddcddddddddddddddddcddc
+cddcddddddddddddddddcddc
+cddcddddddddddddddddcddc
+cbdcddddddddddddddddcdbc
+ccbbbbbbbbbbbbbbbbbbbbcc
+ccbbbbbbbbbbbbbbbbbbbbcc
+cccccccccccccccccccccccc
+..cbbc............cbbc..
+`;
+            case "image1":
+            case "SOFA":return img`
+...cccccccccccccccccc...
+..cbddddddddddddddddbc..
+.cddddddddddddddddddddc.
+.cddbbbbbbbbbbbbbbbbddc.
+.cdbbbbbbbbbbbbbbbbbbdc.
+.cdbbbbbbbbbbbbbbbbbbdc.
+cbbbccccccccccccccccbbbc
+cddcbddddddddddddddbcddc
+cddcddddddddddddddddcddc
+cddcddddddddddddddddcddc
+cddcddddddddddddddddcddc
+cbdcddddddddddddddddcdbc
+ccbbbbbbbbbbbbbbbbbbbbcc
+ccbbbbbbbbbbbbbbbbbbbbcc
+cccccccccccccccccccccccc
+..cbbc............cbbc..
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
